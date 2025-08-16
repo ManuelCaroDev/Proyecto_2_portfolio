4e2a4fb6da3625a0ce11c0573d953252089a4e57
ManuelCaroDev <manuelcaro.dev@gmail.com>
2025-08-16 22:15:12 +0200

    Actualizar Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,9 +5,9 @@ export const Footer = () => `
 
 <h2>Contact</h2>
 <div>
-${Button("./public/icons/instagram.png", "Instagram", "https://www.instagram.com/manuelcarotrigo/")}
-${Button("./public/icons/github.png", "GitHub", "https://github.com/ManuelCaroDev")}
-${Button("./public/icons/linkedin.png", "LinkedIn", "https://es.linkedin.com/in/manuel-caro-trigo-556721317")}
-${Button("./public/icons/gmail.png", "Mail", "mailto:manuelcaro.dev@gmail.com")}
+${Button("/icons/instagram.png", "Instagram", "https://www.instagram.com/manuelcarotrigo/")}
+${Button("/icons/github.png", "GitHub", "https://github.com/ManuelCaroDev")}
+${Button("/icons/linkedin.png", "LinkedIn", "https://es.linkedin.com/in/manuel-caro-trigo-556721317")}
+${Button("/icons/gmail.png", "Mail", "mailto:manuelcaro.dev@gmail.com")}
 </div>
 `;
